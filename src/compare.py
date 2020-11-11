7881b0fe4d778a3d97fff8154cef95dda6969654
Iain Forbes <i.forbes77@gmail.com>
2020-11-11 17:56:38 +0000

    Added last part of if, to return if equal

--- src/compare.py
+++ src/compare.py
@@ -3,5 +3,7 @@ def compare(num_1, num_2):
         return (f'{num_1} is greater than {num_2}')
     elif num_1 < num_2:
         return (f'{num_1} is less than {num_2}')
+    elif num_1 == num_2:
+        return (f'{num_1} is equal to {num_2}')
     
         
--- tests/compare_test.py
+++ tests/compare_test.py
@@ -9,3 +9,6 @@ class TestCompare(unittest.TestCase):
     
     def test_compare_3_5_returns_3_is_less_than_5(self):
         self.assertEqual("3 is less than 5", compare(3, 5))
+
+    def test_compare_10_10_returns_10_is_equal_to_10(self):
+        self.assertEqual("10 is equal to 10", compare(10, 10))
